208e07fb43e26b196aeb3dc74f64267449c422d3
Taylor Grote <grotese@gmail.com>
2021-04-16 13:19:07 -0500

    Add Animation Handler for the Camera State Machine

---
+++ Ostara2021_Unity/Assets/Scripts/CameraStateMachineAnimatorHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using rho;
+using UnityEngine;
+
+public class CameraStateMachineAnimatorHandler : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+
+    [Header("State Values")]
+    [SerializeField] StateScriptableObject _start;
+    [SerializeField] StateScriptableObject _instructions;
+    [SerializeField] StateScriptableObject _gameMode;
+    [SerializeField] StateScriptableObject _levelComplete;
+
+    // Compare State Values and Trigger Appropriate Animation event when state changes
+    public void OnStateChanged(StateScriptableObject oldState, StateScriptableObject newState)
+    {
+        if (newState == _start)
+        {
+            _animator.SetTrigger("ToStart");
+        }
+        else if (newState == _instructions)
+        {
+            _animator.SetTrigger("ToInstructions");
+        }
+        else if (newState == _gameMode)
+        {
+            _animator.SetTrigger("ToGameMode");
+        }
+        else if (newState == _levelComplete)
+        {
+            _animator.SetTrigger("ToLevelComplete");
+        }
+    }
+}
