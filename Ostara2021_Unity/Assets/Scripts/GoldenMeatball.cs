557dc2cb52feb35b137fbd189b00075e86990669
Price Comstock <price.comstock@gmail.com>
2021-04-18 14:55:43 -0400

    speedrun type timer

--- Ostara2021_Unity/Assets/Scripts/GoldenMeatball.cs
+++ Ostara2021_Unity/Assets/Scripts/GoldenMeatball.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GoldenMeatball : MonoBehaviour
 {
+    [SerializeField] rho.ExternalVariable<bool> _isMeatballTouched;
+    [SerializeField] rho.ExternalVariable<float> _meatballTimeSplit;
     [SerializeField] rho.ExternalVariable<bool> _isMeatballRun;
+    [SerializeField] rho.ExternalVariable<float> _timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,12 @@ public class GoldenMeatball : MonoBehaviour
         if (col.gameObject.CompareTag("goldenmeatballgoal"))
         {
             _isMeatballRun.Value = true;
+        }
+
+        if (!_isMeatballTouched.Value && col.gameObject.CompareTag("pasta"))
+        {
+            _meatballTimeSplit.Value = _timer.Value;
+            _isMeatballTouched.Value = true;
         }
     }
 }
--- Ostara2021_Unity/Assets/Scripts/UI/TimerRenderer.cs
+++ Ostara2021_Unity/Assets/Scripts/UI/TimerRenderer.cs
@@ -6,16 +6,25 @@ using UnityEngine.UI;
 public class TimerRenderer : MonoBehaviour
 {
     [SerializeField] rho.ExternalVariable<float> _timerValue;
+    [SerializeField] rho.ExternalVariable<float> _meatballTimeSplit;
     [SerializeField] rho.ExternalVariable<bool> _isMeatballRun;
-    [SerializeField] Text _text;
+    [SerializeField] rho.ExternalVariable<bool> _isMeatballTouched;
+    [SerializeField] Text _runTypeText;
+    [SerializeField] Text _meatballSplitText;
+    [SerializeField] Text _totalText;
     [SerializeField] Color _standardColor;
+    [SerializeField] Color _translucentColor;
     [SerializeField] Color _meatballColor;
     // Update is called once per frame
     void Update()
     {
-        string formattedTime = _timerValue.Value.ToString("0.00");
-        string runType = _isMeatballRun.Value ? "meatball%" : "any%";
-        _text.color = _isMeatballRun.Value ? _standardColor : _meatballColor;
-        _text.text = $"{runType}: {formattedTime}";
+        _runTypeText.text = _isMeatballRun.Value ? "meatball%" : "any%";
+        _runTypeText.color = _isMeatballRun.Value ? _standardColor : _meatballColor;
+
+        _meatballSplitText.text = "(optional) Get Meatball: " + (_isMeatballTouched ? _meatballTimeSplit.Value.ToString("0.00") : "----");
+        _meatballSplitText.color = _isMeatballTouched ? _meatballColor : _translucentColor;
+        _meatballSplitText.fontStyle = FontStyle.Bold;
+
+        _totalText.text = "Finish: " + _timerValue.Value.ToString("0.00");
     }
 }
