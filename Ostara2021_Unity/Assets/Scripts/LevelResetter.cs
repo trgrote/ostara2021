bdc7a52ad801e0b784efda05352ad1e908a7b893
Taylor Grote <grotese@gmail.com>
2021-04-18 13:05:48 -0500

    Add Level Resetter

---
+++ Ostara2021_Unity/Assets/Scripts/LevelResetter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelResetter : MonoBehaviour
+{
+    [SerializeField] GameObject _resettable;
+    [SerializeField] rho.StateVariable _stateRef;
+    [SerializeField] rho.StateScriptableObject _start;
+    GameObject _currentResettable;
+
+    public void OnNoodleCameraSpawn(rho.ExternalVariable<rho.StateScriptableObject> sender, rho.StateScriptableObject oldValue, rho.StateScriptableObject newValue)
+    {
+        if (newValue == _start)
+        {
+            if (_currentResettable)
+            {
+                Destroy(_currentResettable);
+            }
+
+            _currentResettable = Instantiate(_resettable);
+        }
+    }
+
+    void OnEnable()
+    {
+        _stateRef.Changed += OnNoodleCameraSpawn;
+        OnNoodleCameraSpawn(_stateRef, null, _stateRef.Value);
+    }
+
+    void OnDisable()
+    {
+        _stateRef.Changed -= OnNoodleCameraSpawn;
+    }
+}
